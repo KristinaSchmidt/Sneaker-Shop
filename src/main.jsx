68488a50174f11bd11b3028655fe1c7e0892ffc8
KristinaSchmidt <kristina_schmidt21@web.de>
2025-08-22 11:15:04 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
     </BrowserRouter>
-  </React.StrictMode>
 );
