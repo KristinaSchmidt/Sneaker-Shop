d5d12f4ee3836adadbcad5cfbe300ba9a22ed536
KristinaSchmidt <kristina_schmidt21@web.de>
2025-08-24 17:21:09 +0200

    News finish

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -1,14 +1,13 @@
 import styles from "./styles.module.css";
 import SRC from "../../assets/Banner.svg";
 
-function News(){
-    return (
-        <section className={styles.wrap}>
-            <div className={styles.track}>
-                <img className={styles.banner} src={SRC} alt="Shop News"></img>
-               
-            </div>
-        </section>
-    )
+function News() {
+  return (
+    <section className={styles.wrap}>
+      <div className={styles.track}>
+        <img className={styles.banner} src={SRC} alt="Shop News" />
+      </div>
+    </section>
+  );
 }
 export default News;
